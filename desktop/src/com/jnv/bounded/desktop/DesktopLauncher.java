5d2cf1eeff0dcf9aa957204868bf7b08b96feac5
Your Name <vwbluetv@yahoo.com>
2015-08-23 20:28:04 -0700

    Fixed iOS and Desktop Launcher

--- desktop/src/com/jnv/bounded/desktop/DesktopLauncher.java
+++ desktop/src/com/jnv/bounded/desktop/DesktopLauncher.java
@@ -2,6 +2,7 @@ package com.jnv.bounded.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jnv.bounded.main.Bounded;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
